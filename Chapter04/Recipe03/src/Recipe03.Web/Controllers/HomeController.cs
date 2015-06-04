a6d4e07f328a00715eb77b6e74093f4840f360a7
John Ciliberti <johnciliberti@users.noreply.github.com>
2015-06-03 21:18:55 -0400

    Added Recipe 4-3

---
+++ Chapter04/Recipe03/src/Recipe03.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Mvc;
+using Recipe03.Web.Models;
+
+namespace Recipe03.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Contact Us.";
+            var model = new Contact { AllowContactAboutOffers = true };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Contact(Contact model)
+        {
+            ViewBag.Message = "Contact Us.";
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", model);
+            }
+            return View(model);
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
---
+++ Chapter04/Recipe03/src/Recipe03.Web/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recipe03.Web.Models
+{
+    public class Contact
+    {
+
+        public string Name { get; set; }
+
+
+        public string Email { get; set; }
+
+        
+        public string Phone { get; set; }
+
+        [Display(Name="Allow Us to send you SPAM?")]
+        public bool AllowContactAboutOffers { get; set; }
+
+        public string FavoriteColor { get; set; }
+    }
+}
