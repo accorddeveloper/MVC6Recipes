0ab6c765f3f2e5c6082db543afdb7bfde70797a2
John Ciliberti <johnciliberti@users.noreply.github.com>
2015-07-20 21:49:11 -0400

    Chapter 5 Recipes

---
+++ Chapter05/Recipe03/src/Recipe03.Service/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Recipe03.Service
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddWebApiConventions();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            app.UseStaticFiles();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes => {
+                routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+
+        }
+    }
+}
---
+++ Chapter05/Recipe05/src/Recipe05.Web/Services/HitCounterService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Framework.ConfigurationModel;
+
+namespace Recipe05.Web.Services
+{
+    public class HitCounterService
+    {
+        public int UpdateCount()
+        {
+            var config = new Configuration();
+            config.Add(new MemoryConfigurationSource());
+            var KEY = "count";
+            var count = 1;
+            
+            var value = config.Get(KEY);
+            if (value != null)
+            {
+                count = int.Parse(value);
+                count++;
+            }
+            config.Set(KEY, count.ToString());
+            return count;
+
+        }
+    }
+}
